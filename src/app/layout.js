90fd7da430de1fd53e620f42a5c057f0baa7cf1d
Minh Quoc <minhquoc0605@gmail.com>
2024-01-28 00:20:53 +0700

    Ad

--- src/app/layout.js
+++ src/app/layout.js
@@ -66,13 +66,13 @@ export default function RootLayout({ children }) {
 
         )}
       >
-        <Script id="theme-switcher" strategy="beforeInteractive">
+        {/* <Script id="theme-switcher" strategy="beforeInteractive">
           {`if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
   } else {
     document.documentElement.classList.remove('dark')
   }`}
-        </Script>
+        </Script> */}
 
 
         <Header />
